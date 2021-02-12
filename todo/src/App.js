c90c04e7903bc8f025a38afb11e66e301f552cdc
Zane Nichols <nichols.zane16@gmail.com>
2021-02-11 20:51:57 -0600

    Making the final push

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,29 +1,58 @@
-// import logo from './logo.svg';
-import {useState} from 'react';
-
-import logo from './tek_logo.png';
+import React, { useState, useEffect } from "react";
 import './App.css';
-import Todo from './Components/Todo';
+//Importing Components
+import Form from "./components/form";
+import TodoList from "./components/todolist";
+
 
 function App() {
 
-  const [todos, setTodos] = useState(["eat","sleep","code","pray","repeat"]);  //sample todo items.  these will need to be changed in your app.  Just filler todos, although they are quite important!
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-         <br/>
-          <p>Create a Todo List App</p>
-          <Todo item="1"/>
-          <Todo item="2"/>
-          <Todo item="3"/>
-          {todos.map( (t,i) => <Todo key={i} item={t}/>)}
-        </div>
-      </header>
-    </div>
-  );
-}
+  useEffect(() => {
+    filteredHandler(); 
+  }, [todos, status]);
+
+    const filteredHandler = () => {
+      switch(status){
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true));
+          break;
+        case 'incomplete':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    }
+    return (
+      <div className="App">
+        <header>
+          <h1>The Todo List</h1>
+        </header>
+        <Form 
+        inputText={inputText}
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+         />
+  
+        <TodoList 
+        setTodos={setTodos} 
+        todos={todos} 
+        filteredTodos={filteredTodos}/>
+      </div>
+    )};
+  
+    
+
+    
+
+
 
 export default App;
